4a645260640eaa7e0e43034247a24f819e9f9cee
Jeff10B <jobeltran@estudiante.uniajc.edu.co>
2025-09-16 20:24:33 -0500

    segundo commit

--- src/vista/vistaEstudiante.java
+++ src/vista/vistaEstudiante.java
@@ -1,22 +1,18 @@
 package vista;
-
 import java.util.List;
 
 import modelo.Estudiante;
-
-public class vistaEstudiante {
-  public void mostrarInformacionEstudiantes(List<Estudiante> estudiantes) {
-
-    // for (Estudiante estudiante : estudiantes) {
-    // System.out.println("Nombre: " + estudiante.getNombre());
-    // System.out.println("Edad: " + estudiante.getEdad());
-    // }
-
-    estudiantes.forEach(estudiante -> {
-      System.out.println("Nombre: " + estudiante.getNombre());
-      System.out.println("Edad: " + estudiante.getEdad());
-    });
-
-  }
+public class VistaEstudiante {
+    public void mostrarDetallesEstudiante(List<Estudiante> estudiantes) {
+       /*  for (Estudiante estudiante : estudiantes) {
+            System.out.println("Detalles del Estudiante:");
+            System.out.println("Nombre: " + estudiante.getNombre());
+            System.out.println("Edad: " + estudiante.getEdad());
+        }*/
+        estudiantes.forEach(estudiante -> {
+            System.out.println("Nombre: " + estudiante.getNombre() + " - " + "Edad: " + estudiante.getEdad());
+        });
+    }
 }
+
 
