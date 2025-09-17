85b57184115025ff4dcf5e90b189289945da504c
AlejaRivera11 <ariveram@estudiante.uniajc.edu.co>
2025-09-16 20:01:18 -0500

    Se agrega la vista y la clase prueba

--- src/App.java
+++ src/App.java
@@ -6,6 +6,16 @@ public class App {
   public static void main(String[] args) throws Exception {
     System.out.println("MVC Estudiantes bienvienido!");
     
+    Estudiante estudiante1 = new Estudiante("Alejandra", 21);
+    VistaEstudiante vista = new VistaEstudiante();
+    ControladorEstudiante controlador = new ControladorEstudiante(vista);
+     
+    controlador.agregarEstudiante(estudiante1);
+
+    controlador.actualizarVista();
     
+
+    
+
   }
 }
