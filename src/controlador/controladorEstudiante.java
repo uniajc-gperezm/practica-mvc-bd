084214246fa9a6e6b61c30d8b92a43bf8095783f
Jeff10B <jobeltran@estudiante.uniajc.edu.co>
2025-09-16 19:42:58 -0500

    Mi rama creada

---
+++ src/controlador/controladorEstudiante.java
@@ -0,0 +1,28 @@
+package controlador;
+
+
+
+import modelo.Estudiante;
+import vista.vistaEstudiante;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class controladorEstudiante {
+  private vistaEstudiante vista;
+  private List<Estudiante> estudiantes;
+
+  public controladorEstudiante(vistaEstudiante vista) {
+    this.vista = vista;
+    this.estudiantes = new ArrayList<>();
+  }
+
+  public void actualizarVista() {
+    vista.mostrarInformacionEstudiantes(estudiantes);
+  }
+
+  public void agregarEstudiante(Estudiante estudiante) {
+    estudiantes.add(estudiante);
+    actualizarVista();
+  }
+}
---
+++ src/vista/vistaEstudiante.java
@@ -0,0 +1,22 @@
+package vista;
+
+import java.util.List;
+
+import modelo.Estudiante;
+
+public class vistaEstudiante {
+  public void mostrarInformacionEstudiantes(List<Estudiante> estudiantes) {
+
+    // for (Estudiante estudiante : estudiantes) {
+    // System.out.println("Nombre: " + estudiante.getNombre());
+    // System.out.println("Edad: " + estudiante.getEdad());
+    // }
+
+    estudiantes.forEach(estudiante -> {
+      System.out.println("Nombre: " + estudiante.getNombre());
+      System.out.println("Edad: " + estudiante.getEdad());
+    });
+
+  }
+}
+
