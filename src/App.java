e6ced6bbe7dfd0075918a0256e7d038122f7d4cb
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2025-09-16 19:29:50 -0500

    Creando clases modelo, vista y controlador para la entidad estudiante.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
+import modelo.*;
+import controlador.*;
+import vista.*;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) throws Exception {
+    System.out.println("MVC Estudiantes bienvienido!");
+
+  }
 }
