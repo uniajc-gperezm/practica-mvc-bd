b52aae14fb130083eb7b14269ffce373276ab9e4
samir <samir_armero16@hotmail.com>
2025-09-16 19:59:39 -0500

    Actualizacion

--- src/App.java
+++ src/App.java
@@ -1,22 +1,25 @@
 import java.util.Scanner;
 
-import Controlador.ControladorEstudiante;
+import Controlador.*;
 import Modelo.Estudiante;
 import Vista.VistaEstudiante;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("********** Aplicacion MVC - Estudiante **********");
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Ingrese el nombre del estudiante: ");
-        String nombre = scanner.nextLine();
-        System.out.print("Ingrese la edad del estudiante: ");
-        int edad = scanner.nextInt();
-        Estudiante modelo = new Estudiante(nombre, edad);
+        Estudiante modelo = new Estudiante("", 0);
         VistaEstudiante vista = new VistaEstudiante();
         ControladorEstudiante controlador = new ControladorEstudiante(modelo, vista);
+
+        System.out.print("Ingrese el nombre del estudiante: ");
+        String nombre = scanner.nextLine();
+
+        System.out.print("Ingrese la edad del estudiante: ");
+        int edad = scanner.nextInt();
+
+        Estudiante estudiante = new Estudiante(nombre, edad);
+        controlador.agregarEstudiante(estudiante);
         controlador.actualizarVista();
-        // scanner.close();
         scanner.close();
     }
 }
